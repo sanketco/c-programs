2047849e370d833fb21f31771751f63f2f70485d
Sanket Khandelwal <39990230+teknas07@users.noreply.github.com>
2019-10-10 18:00:05 +0530

    Update 6.18.c

--- 6.18.c
+++ 6.18.c
@@ -6,7 +6,7 @@ For example: 145 = 1! + 4! + 5! = 1 + 24 + 120 = 145
 
 #include<stdio.h>
 int main(){
-  int num,i,f,r,sum=0,temp;
+  int num,i,f,r,s=0,temp;
 
   printf("Enter a number: ");
   scanf("%d",&num);
@@ -20,10 +20,10 @@ int main(){
          f=f*i;
         i++;
       }
-      sum=sum+f;
+      s=s+f;
       num=num/10;
   }
-  if(sum==temp)
+  if(s==temp)
       printf("%d is a strong number",temp);
   else
       printf("%d is not a strong number",temp);
